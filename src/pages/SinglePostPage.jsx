5dfcb7778de9043e0c323205f5a51a326703f947
Abass Koyang <abasskoyang12345@gmail.com>
2024-01-22 02:36:28 +0930

    added comment section to single post page

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -4,28 +4,34 @@ import supabase from '../config/supabse';
 
 const SinglePostPage = () => {
     const postId = useParams();
-    console.log(postId);
     const fetchSinglePost = async () => {
-        const {data, error} = await supabase.from('posts').select('*').eq('id', postId.id);
-        if(!error) return data;
+        const {data, error} = await supabase.from('posts').select('*').eq('id', postId.id).single();
+        if(!error){
+            console.log(data)
+            return data
+        }
         if(error){
             console.log(error);
             throw error;
         }
     }
-    const {data:post, error, isLoading, isError} = useQuery({queryKey: ['singlepost'], queryFn: fetchSinglePost})
+    const {data:post, error, isLoading} = useQuery({queryKey: ['singlepost'], queryFn: fetchSinglePost})
 
   return (
     <section className="single-post-page">
-        {error && (<p>Error fetching post</p>)}
-
+        {error && (<p>Error fetching post {error.message}</p>)}
+        {isLoading && (<p>Loading..</p>)}
         {post && 
             (
-            <div>
+            <div className='single-post'>
             <h2>{post.title}</h2>
             <p>{post.content}</p>
             <p>likes: {post.likes}</p>
             <h3>Comments</h3>
+            <form >
+                <textarea name="commentbox" id="" cols="30" rows="15"></textarea>
+                <button type='button'>Submit comment</button>
+            </form>
             </div>)
             }
     </section>
